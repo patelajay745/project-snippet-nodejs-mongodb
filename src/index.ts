9f50add2500fe8524b956b766e37cbd2afffef93
Ajay Patel <patel.ajay745@gmail.com>
2025-06-20 20:50:27 -0400

    used http for app

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import 'dotenv/config'
 import connectDB from "@/db"
 import app from './app';
 import { env } from '@/validators/env';
+import http from 'http'
 
 const PORT = env.PORT ?? 8000
 
-connectDB().then(() => app.listen(PORT, () => {
+const httpServer = http.createServer(app)
+
+connectDB().then(() => httpServer.listen(PORT, () => {
     console.log(`server running on ${env.BASEURL}:${PORT}`)
 }))
 
