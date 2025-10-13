db9238d01f4f221312d579afa5560133d2e92e5d
Ajay Patel <patel.ajay745@gmail.com>
2025-10-13 07:13:02 -0400

    refactor: replace env imports with process.env, update default port, add docker-compose config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,8 @@
-
-import { env } from "@/validators/env"
 import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI!)
         console.log("MongoDB connected successfully")
 
     } catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config'
 import connectDB from "@/db"
 import app from './app';
-import { env } from '@/validators/env';
+
 import http from 'http'
 
-const PORT = env.PORT ?? 8000
+const PORT = process.env.PORT ?? 8082
 
 const httpServer = http.createServer(app)
 
 connectDB().then(() => httpServer.listen(PORT, () => {
-    console.log(`server running on ${env.BASEURL}:${PORT}`)
+    console.log(`server running on ${process.env.BASEURL}:${PORT}`)
 }))
 
